88bf96c1eddf60043377a08613fb5036fbf136e6
Anya <xxxuko@gmail.com>
2024-05-16 22:46:16 +0300

    fix port

--- bot_app/webhook.py
+++ bot_app/webhook.py
@@ -62,7 +62,7 @@ if __name__ == "__main__":
         web.run_app(
             app,
             host="0.0.0.0",
-            port=8080
+            port=8002
         )
     except KeyboardInterrupt:
         print("Shutting down")
