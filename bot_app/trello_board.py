9bdbb67b5f2106d9765e866518d2034765b1115e
Anya <xxxuko@gmail.com>
2024-05-16 16:19:02 +0300

    add check_board_exists function that chech if bord doesnt exist - creates new

--- bot_app/trello_board.py
+++ bot_app/trello_board.py
@@ -10,6 +10,20 @@ TRELLO_APY_KEY = os.getenv("TRELLO_APY_KEY")
 TRELLO_TOKEN = os.getenv("TRELLO_TOKEN")
 
 
+def check_board_exists() -> bool:
+    url = "https://api.trello.com/1/members/me/boards"
+    query = {
+        "key": TRELLO_APY_KEY,
+        "token": TRELLO_TOKEN,
+    }
+    response = requests.get(url, params=query)
+    boards = response.json()
+    for board in boards:
+        if board["name"] == "Trello_Tg_Bot_Board":
+            return True
+    return False
+
+
 def create_board():
     url = "https://api.trello.com/1/boards/"
     query = {
@@ -36,12 +50,15 @@ def create_list(board_id, list_name):
 
 
 def setup_trello_board():
-    board_id = create_board()
-    list_names = ["Done", "InProgress"]
-    for name in list_names:
-        create_list(board_id, name)
-    print(f"Created board with id {board_id}; "
-          f"columns: {', '.join(list_names)}")
+    if not check_board_exists():
+        board_id = create_board()
+        list_names = ["Done", "InProgress"]
+        for name in list_names:
+            create_list(board_id, name)
+        print(f"Created board with id {board_id}; "
+              f"columns: {', '.join(list_names)}")
+    else:
+        print("Board already exists, skipping creation.")
 
 
 if __name__ == "__main__":
