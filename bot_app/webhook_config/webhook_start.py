14bd1021dcf9a1c01adeda32bd27500fd17a9ebe
Anya <xxxuko@gmail.com>
2024-05-17 23:11:53 +0300

    try to fix trello webhook

--- bot_app/webhook_config/webhook_start.py
+++ bot_app/webhook_config/webhook_start.py
@@ -61,9 +61,14 @@ async def handle_trello_webhook(request):
     return web.Response(status=200)
 
 
+async def accept_trello_webhook(request):
+    return web.Response(text="OK")
+
+
 def setup_webhook():
     app.router.add_post("/trello-webhook", handle_trello_webhook)
+    app.router.add_head("/trello-webhook", accept_trello_webhook)
     app.router.add_post(f"/{BOT_TOKEN}", handle_bot_webhook)
-    app.router.add_get("/", handle_get)
+
     app.on_startup.append(on_startup)
     return app
