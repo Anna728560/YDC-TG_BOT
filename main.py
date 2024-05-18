8afba563f7adbb368ff6c8986b8ce0e05729eec7
Anya <xxxuko@gmail.com>
2024-05-18 20:45:35 +0300

    update main

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 import sys
 from aiohttp import web
@@ -9,22 +10,33 @@ from bot_app.webhook_config.webhook_start import setup_webhook
 
 logger = logging.getLogger()
 
-if __name__ == "__main__":
-    try:
-        logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-        app = setup_webhook()
-        web.run_app(
-            app,
-            host="0.0.0.0",
-            port=3009
-        )
-        bord_id = setup_trello_board()
-        logger.info(f"Board ID: {bord_id}")
-        response = set_trello_webhook(bord_id)
-        if response.status_code == 200:
-            logger.info("Webhook created successfully")
-        else:
-            logger.info(f"Error creating webhook: {response.text} \n")
 
+async def main():
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    app = setup_webhook()
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, host="0.0.0.0", port=3009)
+    await site.start()
+    logger.info("Webhook server started")
+
+    board_id = await setup_trello_board()
+    logger.info(f"Board ID: {board_id}")
+
+    response = await set_trello_webhook(board_id)
+    if response.status == 200:
+        logger.info("Webhook created successfully")
+    else:
+        response_text = await response.text()
+        logger.info(f"Error creating webhook: {response_text}")
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(main())
+        loop.run_forever()
     except KeyboardInterrupt:
-        print("Shutting down")
+        logger.info("Shutting down")
+    finally:
+        loop.close()
